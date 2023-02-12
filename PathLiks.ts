c097423a0cf560025838566207c88b2edd1a46ab
nikogoli <k_m_naoto@yahoo.co.jp>
2023-02-13 03:05:55 +0900

    delete

--- PathLiks.ts
+++
@@ -1,8 +0,0 @@
-import * as DenoPath from "https://deno.land/std@0.170.0/path/mod.ts"
-import { PurePathLike } from "./PurePathLike.ts"
-
-export class PathLike extends PurePathLike {
-  cwd() {
-      return new PathLike(Deno.cwd())
-  }
-}
